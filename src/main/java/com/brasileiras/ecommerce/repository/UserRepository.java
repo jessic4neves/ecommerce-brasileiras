919a4de5a84ab6a6eb9e7177db4aaed422dfa19d
Bowie <jessic4neves@gmail.com>
2025-05-25 16:30:43 -0300

    feat: Finalização dos getters e setters

--- src/main/java/com/brasileiras/ecommerce/repository/UserRepository.java
+++ src/main/java/com/brasileiras/ecommerce/repository/UserRepository.java
@@ -3,5 +3,8 @@ package com.brasileiras.ecommerce.repository;
 import com.brasileiras.ecommerce.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface UserRepository extends JpaRepository<User, Long> {
+    Optional<User> findByEmail(String email);
 }
