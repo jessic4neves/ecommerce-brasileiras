84acdfd037bc2c9d68f57c97187cecc20e039b2d
Bowie <jessic4neves@gmail.com>
2025-05-25 17:03:10 -0300

    feat: Finalização dos getters e setters

--- src/main/java/com/brasileiras/ecommerce/repository/AccountReceivableRepository.java
+++ src/main/java/com/brasileiras/ecommerce/repository/AccountReceivableRepository.java
@@ -1,7 +1,7 @@
 package com.brasileiras.ecommerce.repository;
 
-import com.brasileiras.ecommerce.model.AccountReceivable;
+import com.brasileiras.ecommerce.model.AccountReceive;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface AccountReceivableRepository extends JpaRepository<AccountReceivable, Long> {
+public interface AccountReceivableRepository extends JpaRepository<AccountReceive, Long> {
 }
